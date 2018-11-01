18d6a99cab267573844761f1805d62393917dde4
Volha Valasevich <Volha_Valasevich@epam.com>
2018-11-01 10:17:44 +0300

    add yargs

--- index.js
+++ index.js
@@ -8,11 +8,24 @@ const path = require('path');
 const { parseReport } = require('./util/dataParser');
 const logger = require('./util/logger').logger;
 const spreadsheetIdPath = path.join(__dirname, 'resources', 'spreadsheetId.json');
+const yargs = require('yargs')
+        .usage('Cucumber reporter for Google Spreadsheets.\nUsage: spreadsheets <path to report.json>')
+        .option('r', {
+            alias: 'report',
+            describe: 'path to cucumber report.json',
+            type: 'string',
+            demand: true
+        }).argv;
 
-async function main(report) {
-    let spreadsheetId;
+async function main(reportPath) {
+    let report, spreadsheetId;
     const auth = await authorize(creds);
     const steps = new stepFunctions(auth);
+    try {
+        report = require(reportPath); 
+    } catch (err) {
+        throw new Error(`Cannot read report file - ${err.message}`);
+    }
     if (!fs.existsSync(spreadsheetIdPath)) {
         spreadsheetId = await steps.createNewSpreadsheet('test');
         fs.writeFileSync(spreadsheetIdPath, JSON.stringify({ id: spreadsheetId }));
@@ -25,8 +38,8 @@ async function main(report) {
     logger.info(`Successfully updated test data on https://docs.google.com/spreadsheets/d/${spreadsheetId}`);
 }
 
-main().catch((err) => {
-    logger.error(err);
+main(yargs.report).catch((err) => {
+    logger.error(`${err.message}\n${err.stack}`);
     if (err === 'Requested entity was not found.') {
         logger.info('A new spreadsheet will be created.');
         fs.unlinkSync(spreadsheetIdPath);
